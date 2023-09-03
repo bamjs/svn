7eddb6f4cbeb03658c70e38542ca03e330345b22
jmuthyala <jeevan.muthyala@infor.com>
2023-09-03 11:01:31 +0530

    added favicon.ico and bg images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,24 @@ import { MongoDbService } from 'src/services/mongo-db.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'invite';
+  title = 'Srikanth Weds Neelima';
+  photo = '../assets/single.jpg'
+  photos=['../assets/single.jpg','../assets/group_new.jpg','../assets/child.jpg']
   loader$
+  index = 0
   constructor(
     private mongoDb:MongoDbService,
     private commonService: CommonService
     ){
       this.loader$ = this.commonService.loader
+      setInterval(()=>{
+        if (this.index < this.photos.length) {
+          this.photo = this.photos[this.index++]
+        }else{
+          this.index =0
+        }
+
+      },2000)
   }
 
 }
